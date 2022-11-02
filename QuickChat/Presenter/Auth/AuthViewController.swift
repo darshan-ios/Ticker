e32d6b76dcbefc1deaddf8eb87bc889aae31a185
darshan <iosdarshan7@gmail.com>
2022-11-02 19:44:46 +0530

    Ticker-own

--- QuickChat/Presenter/Auth/AuthViewController.swift
+++ QuickChat/Presenter/Auth/AuthViewController.swift
@@ -1,24 +1,4 @@
-//  MIT License
 
-//  Copyright (c) 2019 Haik Aslanyan
-
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-
-//  The above copyright notice and this permission notice shall be included in all
-//  copies or substantial portions of the Software.
-
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//  SOFTWARE.
 
 import UIKit
 
@@ -87,8 +67,11 @@ extension AuthViewController {
     manager.register(user: user) {[weak self] response in
       ThemeService.showLoading(false)
       switch response {
-        case .failure: self?.showAlert()
-        case .success: self?.dismiss(animated: true, completion: nil)
+        case .failure:
+          self?.showAlert()
+        case .success:
+          let vc = UIStoryboard.initial(storyboard: UserManager().currentUserID().isNone ? .auth : .conversations)
+            UIApplication.shared.keyWindow?.rootViewController = vc
       }
     }
   }
@@ -114,7 +97,9 @@ extension AuthViewController {
       ThemeService.showLoading(false)
       switch response {
       case .failure: self?.showAlert()
-      case .success: self?.dismiss(animated: true, completion: nil)
+      case .success:
+          let vc = UIStoryboard.initial(storyboard: UserManager().currentUserID().isNone ? .auth : .conversations)
+            UIApplication.shared.keyWindow?.rootViewController = vc
       }
     }
   }
